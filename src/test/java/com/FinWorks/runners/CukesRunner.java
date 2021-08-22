81482827d622aac2e2ae746c4f692ae6a8919f63
Evelin <80602149+Evelin-76@users.noreply.github.com>
2021-08-22 14:12:00 +0200

    Update CukesRunner.java
    tag login or logout

--- src/test/java/com/FinWorks/runners/CukesRunner.java
+++ src/test/java/com/FinWorks/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/FinWorks/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@login or @logout"
 )
 public class CukesRunner {
 }
